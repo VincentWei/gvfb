c0786b8768a22ee4bc1b62d71128c35ff5ae86da
Vincent Wei <vincent@minigui.org>
2020-03-16 17:41:04 +0800

    use msg_log for msg_out

--- src/gvfb_log.h
+++ src/gvfb_log.h
@@ -38,38 +38,37 @@
 #define LEVEL_1 1
 #define LEVEL_2 2
 
-#ifndef msg_out
+#ifdef DEBUG
+void msg_log(const char* fmt, ...);
+#else   /* defined DEBUG */
+#define msg_log(fmt, ...)   \
+    do {                    \
+    } while(0)
+#endif  /* not defined DEBUG */
 
 #ifdef SHOW_DBG_MSG
 #ifdef SHOW_FULL_DBG_MSG
 #define msg_out(_level,...)                                 \
     do {                                                    \
-        fprintf (stderr, "[%s]%d: ", __FILE__, __LINE__);   \
-        fprintf (stderr, __VA_ARGS__);                      \
-        fprintf (stderr, "\n");                             \
+        msg_log ("[%s]%d: ", __FILE__, __LINE__);           \
+        msg_log (__VA_ARGS__);                              \
+        msg_log ("\n");                             \
     }                                                       \
     while (0)
 #else /* SHOW_FULL_DBG_MSG */
 #define msg_out(_level,...)             \
     do {                                \
-        fprintf (stderr, __VA_ARGS__);  \
-        fprintf (stderr, "\n");         \
+        msg_log (__VA_ARGS__);          \
+        msg_log ("\n");                 \
     }                                   \
     while (0)
-#endif /* SHOW_FULL_DBG_MSG */
+#endif /* not defined SHOW_FULL_DBG_MSG */
+
 #else  /* SHOW_DBG_MSG */
+
 #define msg_out(_level,...)
-#endif /* SHOW_DBG_MSG */
 
-#endif
-
-#ifdef DEBUG
-void msg_log(const char* fmt, ...);
-#else
-#define msg_log(fmt, ...)   \
-    do {                    \
-    } while(0)
-#endif
+#endif /* not define SHOW_DBG_MSG */
 
 #endif /* _GVFB_LOG_H_ */
 
